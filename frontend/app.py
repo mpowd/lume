ddd668b96bc27ec3b7291ace21eb15a14557c5ea
mpowd <malte.pulver71@gmail.com>
2025-03-30 05:00:45 +0200

    added streamlit start page

---
+++ frontend/app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_icon='./frontend/favicon.png', layout = "wide")
+
+
+# st.title("Knowledge Base")
+
+# st.write("Welcome to the Knowledge Base Platform!")
