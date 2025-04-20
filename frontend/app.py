9f3eeb27010938d0f6f2eaedb231ed1ee72e7199
mpowd <malte.pulver71@gmail.com>
2025-04-21 00:37:39 +0200

    deleted migrated files

--- frontend/app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_icon='./frontend/favicon.png', layout = "wide")
-
-
-# st.title("Knowledge Base")
-
-# st.write("Welcome to the Knowledge Base Platform!")
